3c1310f1a183525024fe7521132a0513dc5aa418
Ryabova2020 <76618274+EkaEkaEkaRy@users.noreply.github.com>
2024-04-28 01:01:02 +0300

    Update restaurant.js

--- controllers/restaurant.js
+++ controllers/restaurant.js
@@ -35,8 +35,40 @@ exports.find_ad = app.get("", async(req, res) => {
 exports.filter_find_ad = app.post("", async(req, res) => {
     try {
         const {type, cuisine, bill } = req.body;
-        let query = `SELECT name, address, cuisine, bill, image FROM restaurants`;
-        
+        let query = `SELECT DISTINCT name, address, cuisine, bill, image 
+        FROM restaurants, rooms WHERE restaurants.id = rooms.restaurant`;
+        if (type.length == 1 && type[0] == 'зал') query += ` AND rooms.status = true`;
+        if (cuisine.length != 0){
+            query += ` AND (`
+            for (let i = 0; i < cuisine.length; i++) {
+                if (i != 0) query += ` OR `
+                query += `cuisine = '${cuisine[i]}'`
+            }
+            query += `)`
+        }
+        if (bill.length != 0){
+            query += ` AND (`
+            for (let i = 0; i < bill.length; i++) {
+                if (i != 0) query += ` OR `
+                switch (bill[i]) {
+                    case "до 1000":
+                        query += `bill < 1000`
+                        break;
+                    case "1000 - 2000":
+                        query += `bill BETWEEN 1000 AND 2000`
+                        break;
+                    case "2000 - 3000":
+                        query += `bill BETWEEN 2000 AND 3000`
+                        break;
+                    case "от 3000":
+                        query += `bill > 3000`
+                        break;
+                    default:
+                        break;
+                }
+            }
+            query += `)`
+        }
         const findRestaurant = await pool.query(query)
         res.json(findRestaurant["rows"])
         } catch (err) {
